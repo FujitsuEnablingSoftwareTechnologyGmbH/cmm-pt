67753e15ec6283985bc1072c65e5d40a0967e09d
Adrian Czarnecki <adrian.czarnecki@ts.fujitsu.com>
2017-03-06 15:44:39 +0100

    Change log_latency.py  program to object-oriented

--- performance/log_latency.py
+++ performance/log_latency.py
@@ -21,21 +21,171 @@ The public variables are described in basic_configuration.yaml and test_configur
 """
 
 import argparse
+import datetime
 import httplib
-import sys
-import simplejson
-import threading
-import time
-import TokenHandler
-import string
 import random
 import search_logs
+import simplejson
+import string
+import sys
+import threading
+import time
 import yaml
-import datetime
-from Queue import Queue
 from urlparse import urlparse
 from threading import Thread
+import TokenHandler
 from write_logs import create_file, write_line_to_file, serialize_logging
+
+
+TEST_NAME = 'log_latency'
+BULK_URL_PATH = '/v3.0/logs'
+SINGLE_LOG_API_URL_PATH = '/v2.0/log/single'
+
+
+class LogLatency(threading.Thread):
+    def __init__(self, keystone_url, log_api_url, elastic_url, tenant_username, tenant_password, tenant_project,
+                 runtime, thread_num, log_api_type, bulk_size, log_size):
+        threading.Thread.__init__(self)
+        self.keystone_url = keystone_url
+        self.log_api_url = log_api_url
+        self.elastic_url = elastic_url
+        self.tenant_username = tenant_username
+        self.tenant_password = tenant_password
+        self.tenant_project = tenant_project
+        self.runtime = runtime
+        self.thread_num = thread_num
+        self.log_api_type = log_api_type
+        self.bulk_size = bulk_size
+        self.log_size = log_size
+        self.token_handler = TokenHandler.TokenHandler(tenant_username, tenant_password, tenant_project, keystone_url)
+        self.result_file = self.create_result_file()
+
+    def search_logs_in_elastic(self, message, duration, check_rate):
+        """this function search logs entry that contain specified message in database,
+        If function find expected log entry will return 'OK' message,
+        If function could not find all expected log entry in specified time will return FAILED string
+        Parameters:
+            message :searched message in log
+            duration : maximum time with function try to find all log entry
+            check_rate: time that determinate time break between checks
+        """
+        if self.log_api_type == 'bulk':
+            number_of_search_log = self.bulk_size
+        else:
+            number_of_search_log = 1
+        timeout = time.time() + duration
+        while time.time() < timeout:
+            if search_logs.count_logs_by_app_message(message, self.elastic_url)[0] >= number_of_search_log:
+                return 'OK'
+            time.sleep(check_rate)
+        return 'FAILED'
+
+    def check_latency(self, message):
+        """ this send log entry in single ol bul mode  to the log api
+        then invoke function that try to find just sent log entry """
+
+        if self.log_api_type == 'bulk':
+            send_status = self.send_bulk_log(message)
+        else:
+            send_status = self.send_single_log(message)
+        search_status = self.search_logs_in_elastic(message, 200, 0.5)
+        return send_status, search_status
+
+    def send_single_log(self, message):
+        self.get_request_header()
+        conn = httplib.HTTPConnection(urlparse(self.log_api_url).netloc)
+        body = simplejson.dumps({'message': message})
+        conn.request("POST", SINGLE_LOG_API_URL_PATH, body, self.get_request_header())
+        res = conn.getresponse()
+        return res.status
+
+    def get_request_header(self):
+        if self.log_api_type == 'single':
+            headers_post = {'X-Dimensions': 'applicationname:latency_test,environment:productioni',
+                            'X-Application-Type': 'SystemTest'}
+        else:
+            headers_post = {'Content-type': 'application/json', 'X-Auth-Token': self.token_handler.get_valid_token()}
+
+        return headers_post
+
+    def send_bulk_log(self, message):
+        self.get_request_header()
+        conn = httplib.HTTPConnection(urlparse(self.log_api_url).netloc)
+        body = simplejson.dumps({'logs': self.bulk_size * [{'message': message}]})
+        conn.request("POST", BULK_URL_PATH, body, self.get_request_header())
+        res = conn.getresponse()
+        return res.status
+
+    def generate_unique_message(self, message=None):
+        letters = string.ascii_lowercase
+
+        def rand():
+            return ''.join((random.choice(letters) for _ in range(self.log_size)))
+        if not message:
+            message = rand()
+        return message
+
+    def run_latency_test(self):
+        thread_name = threading.currentThread().getName()
+        latency_check_count = 0
+        print("{}: Start Time: {} ".format(thread_name, datetime.datetime.now().strftime("%H:%M:%S.%f")))
+        start_time = time.time()
+        while time.time() < (start_time + self.runtime):
+            check_start_time = time.time()
+            send_status, search_status = self.check_latency(self.generate_unique_message())
+            check_end_time = time.time()
+            latency_check_count += 1
+            self.write_latency_check_result(send_status, search_status, check_start_time, check_end_time,
+                                            thread_name, str(latency_check_count))
+        final_time = time.time()
+        print("-----Test Results-----")
+        print("{}: End Time: {}".format(thread_name, thread_name, datetime.datetime.now().strftime("%H:%M:%S.%f")))
+        print("{}: {} log entries in {} seconds".format(thread_name, latency_check_count, final_time-start_time))
+
+    def write_latency_check_result(self, send_status, search_status, start_time, end_time, name, count):
+        def convert_epoch(epoch_time):
+            return time.strftime('%H:%M:%S', time.localtime(epoch_time))
+        latency = end_time - start_time
+
+        if send_status == 204:
+            if search_status == 'OK':
+                print("{} Time: {}, Send and search injector log OK in: {} seconds!"
+                      .format(name, datetime.datetime.now().strftime("%H:%M:%S.%f"), latency))
+            else:
+                print("{} Time: {}, Send injector log OK but search failed in: {} seconds!"
+                      .format(name, datetime.datetime.now().strftime("%H:%M:%S.%f"), latency))
+            result_line = "{}, {}, OK, {}, {}, {}, {}"\
+                .format(name, count, search_status, convert_epoch(start_time), convert_epoch(end_time), latency, count)
+            serialize_logging(self.result_file, result_line)
+        else:
+            print("{} Time: {}, Send Injector log Failed after: {}"
+                  .format(name, datetime.datetime.now().strftime("%H:%M:%S.%f"), latency)+" seconds!")
+
+    def create_result_file(self):
+        """create file for result then write header line to this file """
+        test_info_line = "Log_api_mode: {}, Number of threads: {}, RunTime: {}, Log_size: {}"\
+                         .format(self.log_api_type, self.thread_num, self.runtime, self.log_size)
+        header_line = "Thread#, Count, Send_Status, Search_Result, Start Time, Stop Time, Latency"
+        if self.log_api_type == 'bulk':
+            test_info_line += ", Number of logs in one Bulk: {}".format(self.bulk_size)
+        res_file = create_file(TEST_NAME)
+        write_line_to_file(res_file, test_info_line)
+        write_line_to_file(res_file, header_line)
+        return res_file
+
+    def run(self):
+        print("Test Start, :" + str(self.thread_num) + " thread(s)")
+        thread_list = []
+        for i in range(self.thread_num):
+            thread = Thread(target=self.run_latency_test)
+            time.sleep(0.3)
+            thread.start()
+            thread_list.append(thread)
+
+        for thread in thread_list:
+            thread.join()
+
+        self.result_file.close()
 
 
 def create_program_argument_parser():
@@ -55,176 +205,36 @@ def create_program_argument_parser():
     parser.add_argument('-log_size', action="store", dest='log_size', type=int)
     return parser.parse_args()
 
-TEST_NAME = 'log_latency'
-BULK_URL_PATH = '/v3.0/logs'
-SINGLE_LOG_API_URL_PATH = '/v2.0/log/single'
-
-
-if len(sys.argv) <= 1:
-    BASIC_CONF = yaml.load(file('basic_configuration.yaml'))
-    TEST_CONF = yaml.load(file('test_configuration.yaml'))
-    KEYSTONE_URL = BASIC_CONF['url']['keystone']
-    LOG_API_URL = BASIC_CONF['url']['log_api_url']
-    ELASTIC_URL = urlparse(BASIC_CONF['url']['elastic_url']).netloc
-    TENANT_USERNAME = BASIC_CONF['users']['tenant_name']
-    TENANT_PASSWORD = BASIC_CONF['users']['tenant_password']
-    TENANT_PROJECT = BASIC_CONF['users']['tenant_project']
-    RUNTIME = TEST_CONF[TEST_NAME]['runtime']
-    THREADS_NUM = TEST_CONF[TEST_NAME]['num_threads']
-    LOG_API_TYPE = TEST_CONF[TEST_NAME]['log_api_type']
-    NUM_Of_LOGS_IN_ONE_BULK = TEST_CONF[TEST_NAME]['num_of_logs_in_one_bulk']
-    LOG_SIZE = TEST_CONF[TEST_NAME]['log_size']
-else:
-    program_argument = create_program_argument_parser()
-    KEYSTONE_URL = program_argument.keystone_url
-    LOG_API_URL = program_argument.log_api_url
-    ELASTIC_URL = urlparse(program_argument.elastic_url).netloc
-    TENANT_USERNAME = program_argument.tenant_name
-    TENANT_PASSWORD = program_argument.tenant_password
-    TENANT_PROJECT = program_argument.tenant_project
-    RUNTIME = program_argument.runtime
-    THREADS_NUM = program_argument.num_threads
-    LOG_API_TYPE = program_argument.log_api_type
-    NUM_Of_LOGS_IN_ONE_BULK = program_argument.num_of_logs_in_one_bulk
-    LOG_SIZE = program_argument.log_size
-
-
-token_handler = TokenHandler.TokenHandler(TENANT_USERNAME, TENANT_PASSWORD, TENANT_PROJECT, KEYSTONE_URL)
-
-headers_post = {"Content-type": "application/json"}
-if LOG_API_TYPE == 'single':
-    headers_post = {"X-Dimensions": "applicationname:latency_test,environment:productioni",
-                    "X-Application-Type": 'SystemTest'}
-
-
-def search_logs_in_elastic(message, duration, check_rate):
-    """this function search logs entry that contain specified message in database,
-    If function find expected log entry will return 'OK' message,
-    If function could not find all expected log entry in specified time will return FAILED string
-    Parameters:
-        message :searched message in log
-        duration : maximum time with function try to find all log entry
-        check_rate: time that determinate time break between checks
-    """
-    if LOG_API_TYPE == 'bulk':
-        number_of_search_log = NUM_Of_LOGS_IN_ONE_BULK
-    else:
-        number_of_search_log = 1
-    timeout = time.time() + duration
-    while time.time() < timeout:
-        if search_logs.count_logs_by_app_message(message, ELASTIC_URL)[0] >= number_of_search_log:
-            return 'OK'
-        time.sleep(check_rate)
-    return 'FAILED'
-
-
-def check_latency(message):
-    """ this send log entry in single ol bul mode  to the log api
-    then invoke function that try to find just sent log entry """
-
-    if LOG_API_TYPE == 'bulk':
-        send_status = send_bulk_log(message)
-    else:
-        send_status = send_single_log(message)
-    search_status = search_logs_in_elastic(message, 200, 0.5)
-    return send_status, search_status
-
-
-def send_single_log(message):
-    generate_valid_token()
-    conn = httplib.HTTPConnection(urlparse(LOG_API_URL).netloc)
-    body = simplejson.dumps({'message': message})
-    conn.request("POST", SINGLE_LOG_API_URL_PATH, body, headers_post)
-    res = conn.getresponse()
-    return res.status
-
-
-def generate_valid_token():
-    token_id = token_handler.get_valid_token()
-    headers_post["X-Auth-Token"] = token_id
-
-
-def send_bulk_log(message):
-    generate_valid_token()
-    conn = httplib.HTTPConnection(urlparse(LOG_API_URL).netloc)
-    body = simplejson.dumps({'logs': NUM_Of_LOGS_IN_ONE_BULK * [{'message': message}]})
-    conn.request("POST", BULK_URL_PATH, body, headers_post)
-    res = conn.getresponse()
-    return res.status
-
-
-def generate_unique_message(message=None, msg_size=50):
-    letters = string.ascii_lowercase
-
-    def rand(size):
-        return ''.join((random.choice(letters) for _ in range(size)))
-    if not message:
-        message = rand(msg_size)
-    return message
-
-
-def run_latency_test(res_file):
-    thread_name = threading.currentThread().getName()
-    latency_check_count = 0
-    print("{}: Start Time: {} ".format(thread_name, datetime.datetime.now().strftime("%H:%M:%S.%f")))
-    start_time = time.time()
-    while time.time() < (start_time + RUNTIME):
-        check_start_time = time.time()
-        send_status, search_status = check_latency(generate_unique_message(msg_size=LOG_SIZE))
-        check_end_time = time.time()
-        latency_check_count += 1
-        write_latency_check_result(res_file, send_status, search_status, check_start_time, check_end_time, thread_name,
-                                   str(latency_check_count))
-
-    final_time = time.time()
-    print("-----Test Results-----")
-    print("{}: End Time: {}".format(thread_name, thread_name, datetime.datetime.now().strftime("%H:%M:%S.%f")))
-    print("{}: {} log entries in {} seconds".format(thread_name, latency_check_count, final_time-start_time))
-
-
-def write_latency_check_result(res_file, send_status, search_status, start_time, end_time, name, count):
-    def convert_epoch(epoch_time):
-        return time.strftime('%H:%M:%S', time.localtime(epoch_time))
-    latency = end_time - start_time
-
-    if send_status == 204:
-        if search_status == 'OK':
-            print("{} Time: {}, Send and search injector log OK in: {} seconds!"
-                  .format(name, datetime.datetime.now().strftime("%H:%M:%S.%f"), latency))
-        else:
-            print("{} Time: {}, Send injector log OK but search failed in: {} seconds!"
-                  .format(name, datetime.datetime.now().strftime("%H:%M:%S.%f"), latency))
-        result_line = "{}, {}, OK, {}, {}, {}, {}"\
-            .format(name, count, search_status, convert_epoch(start_time), convert_epoch(end_time), latency, count)
-        serialize_logging(res_file, result_line)
-    else:
-        print("{} Time: {}, Send Injector log Failed after: {}"
-              .format(name, datetime.datetime.now().strftime("%H:%M:%S.%f"), latency)+" seconds!")
-
-
-def create_result_file():
-    """create file for result then write header line to this file """
-    test_info_line = "Log_api_mode: {}, Number of threads: {}, RunTime: {}, Log_size: {}"\
-                     .format(LOG_API_TYPE, THREADS_NUM, RUNTIME, LOG_SIZE)
-    header_line = "Thread#, Count, Send_Status, Search_Result, Start Time, Stop Time, Latency"
-    if LOG_API_TYPE == 'bulk':
-        test_info_line += ", Number of logs in one Bulk: {}".format(NUM_Of_LOGS_IN_ONE_BULK)
-    res_file = create_file(TEST_NAME)
-    write_line_to_file(res_file, test_info_line)
-    write_line_to_file(res_file, header_line)
-    return res_file
-
 if __name__ == "__main__":
-    print("Test Start, :" + str(THREADS_NUM) + " thread(s)")
-    result_file = create_result_file()
-    q = Queue(THREADS_NUM)
-    thread_list = []
-    for i in range(THREADS_NUM):
-        thread = Thread(target=run_latency_test, args=(result_file,))
-        time.sleep(0.3)
-        thread.start()
-        thread_list.append(thread)
+    if len(sys.argv) <= 1:
+        BASIC_CONF = yaml.load(file('basic_configuration.yaml'))
+        TEST_CONF = yaml.load(file('test_configuration.yaml'))
+        KEYSTONE_URL = BASIC_CONF['url']['keystone']
+        LOG_API_URL = BASIC_CONF['url']['log_api_url']
+        ELASTIC_URL = urlparse(BASIC_CONF['url']['elastic_url']).netloc
+        TENANT_USERNAME = BASIC_CONF['users']['tenant_name']
+        TENANT_PASSWORD = BASIC_CONF['users']['tenant_password']
+        TENANT_PROJECT = BASIC_CONF['users']['tenant_project']
+        RUNTIME = TEST_CONF[TEST_NAME]['runtime']
+        THREADS_NUM = TEST_CONF[TEST_NAME]['num_threads']
+        LOG_API_TYPE = TEST_CONF[TEST_NAME]['log_api_type']
+        NUM_Of_LOGS_IN_ONE_BULK = TEST_CONF[TEST_NAME]['num_of_logs_in_one_bulk']
+        LOG_SIZE = TEST_CONF[TEST_NAME]['log_size']
+    else:
+        program_argument = create_program_argument_parser()
+        KEYSTONE_URL = program_argument.keystone_url
+        LOG_API_URL = program_argument.log_api_url
+        ELASTIC_URL = urlparse(program_argument.elastic_url).netloc
+        TENANT_USERNAME = program_argument.tenant_name
+        TENANT_PASSWORD = program_argument.tenant_password
+        TENANT_PROJECT = program_argument.tenant_project
+        RUNTIME = program_argument.runtime
+        THREADS_NUM = program_argument.num_threads
+        LOG_API_TYPE = program_argument.log_api_type
+        NUM_Of_LOGS_IN_ONE_BULK = program_argument.num_of_logs_in_one_bulk
+        LOG_SIZE = program_argument.log_size
 
-    for thread in thread_list:
-        thread.join()
-    result_file.close()
+    log_latency = LogLatency(KEYSTONE_URL, LOG_API_URL, ELASTIC_URL, TENANT_USERNAME, TENANT_PASSWORD, TENANT_PROJECT,
+                             RUNTIME, THREADS_NUM, LOG_API_TYPE, NUM_Of_LOGS_IN_ONE_BULK, LOG_SIZE)
+
+    log_latency.start()
