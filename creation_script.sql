cf79cacc2a632dcb91c1d85e0c6fa85e6d2b6bea
Oussama Khaznaji <oussama.khaznaji@est.fujitsu.com>
2017-05-08 13:31:39 +0200

    Added the metrics tables (RAM, CPU, Disk usage and Kafka lag)

--- creation_script.sql
+++ creation_script.sql
@@ -25,9 +25,54 @@ CREATE TABLE `Result` (
 	PRIMARY KEY (`resultID`)
 );
 
+CREATE TABLE `cpu_percent` (
+	`cpu_percent_ID`   INT NOT NULL auto_increment,
+	`testCaseID` INT NOT NULL,
+	`value` FLOAT NOT NULL,
+	`time_stamp` DATETIME NOT NULL,
+	`hostname` VARCHAR(100),
+	PRIMARY KEY (`cpu_percent_ID`)
+);
+
+CREATE TABLE `mem_used_mb` (
+	`mem_used_mb_ID` INT NOT NULL auto_increment,
+	`testCaseID` INT NOT NULL,
+	`value` DECIMAL NOT NULL,
+	`time_stamp` DATETIME NOT NULL,
+	`hostname` VARCHAR(100),
+	PRIMARY KEY (`mem_used_mb_ID`)
+);
+
+CREATE TABLE `disk_usage` (
+	`disk_usage_ID` INT NOT NULL auto_increment,
+	`testCaseID` INT NOT NULL,
+	`value_before` DECIMAL,
+	`value_after` DECIMAL,
+	`hostname` VARCHAR(100),
+	PRIMARY KEY (`disk_usage_ID`)
+);
+
+CREATE TABLE `kafka_lag` (
+	`kafka_lag_ID` INT NOT NULL auto_increment,
+	`testCaseID` INT NOT NULL,
+	`time_stamp` DATETIME NOT NULL,
+	`logstash_persister` DECIMAL NOT NULL,
+	`1_metrics` DECIMAL NOT NULL,
+	`transformer_logstash_consumer` DECIMAL NOT NULL,	
+	PRIMARY KEY (`kafka_lag_ID`)
+);
+
 ALTER TABLE `Test` ADD CONSTRAINT `Test_TestCase_fk` FOREIGN KEY (`testCaseID`) REFERENCES `TestCase`(`testCaseID`) ON DELETE CASCADE;
+
+ALTER TABLE `cpu_percent` ADD CONSTRAINT `cpu_percent_TestCase_fk` FOREIGN KEY (`testCaseID`) REFERENCES `TestCase`(`testCaseID`) ON DELETE CASCADE;
+
+ALTER TABLE `mem_used_mb` ADD CONSTRAINT `ram_usage_TestCase_fk` FOREIGN KEY (`testCaseID`) REFERENCES `TestCase`(`testCaseID`) ON DELETE CASCADE;
+
+ALTER TABLE `disk_usage` ADD CONSTRAINT `disk_usage_TestCase_fk` FOREIGN KEY (`testCaseID`) REFERENCES `TestCase`(`testCaseID`) ON DELETE CASCADE;
+
+ALTER TABLE `kafka_lag` ADD CONSTRAINT `kafka_lag_TestCase_fk` FOREIGN KEY (`testCaseID`) REFERENCES `TestCase`(`testCaseID`) ON DELETE CASCADE;
 
 ALTER TABLE `Result` ADD CONSTRAINT `Result_Test_fk` FOREIGN KEY (`testID`) REFERENCES `Test`(`testID`) ON DELETE CASCADE;
 
--- The following line will be deleted once the launcher program is ready
+-- The following line in needed in case some scripts are to be launched individually and not within a test suite
 Insert into TestCase(testCaseID, testSuite, startTime) values ('1', 'default', NOW());
